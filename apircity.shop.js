5222171e0537a780c2ff45945d4534c7c272ea7e
apircity <peter261104@naver.com>
2022-01-15 19:06:40 +0900

    shopping mall UI img X

---
+++ apircity.shop.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Online Shopping</title>
+        <link rel="stylesheet" href="style.css"></link>
+        <script src="src/main.js" defer></script>
+    </head>
+    <body>
+{/* //Logo//->주석 드래그후 ctrl / */}
+        <img scr=""
+    // Buttnos
+    <button class=""><img scr="" alt="" class="imgBtn"></img></button>
+    
+    <button class=""><img scr="" alt="" class="imgBtn"></img></button>
+    
+    <button class=""><img scr="" alt="" class="imgBtn"></img></button>
+
+
+
+
+
+    </body>
+</html>
