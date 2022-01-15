bca3ba7d978607345e5d474d6f47e3bde5e88187
apircity <peter261104@naver.com>
2022-01-16 03:13:53 +0900

    2022.01.16 am 03.13 read.me 수정  apircity.shop 수정

--- apircity.shop.js
+++ apircity.shop.js
@@ -5,21 +5,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Online Shopping</title>
         <link rel="stylesheet" href="style.css"></link>
-        <script src="src/main.js" defer></script>
+        <script src="https://vo.la/phifC" defer></script>
     </head>
     <body>
 {/* //Logo//->주석 드래그후 ctrl / */}
         <img scr=""
     // Buttnos
-    <button class=""><img scr="" alt="" class="imgBtn"></img></button>
+    <button class="btn"><img scr="" alt="tshirt" class="imgBtn"></img></button>
+
+    <button class="btn"><img scr="" alt="pants" class="imgBtn"></img></button>
     
-    <button class=""><img scr="" alt="" class="imgBtn"></img></button>
-    
-    <button class=""><img scr="" alt="" class="imgBtn"></img></button>
-
-
-
-
-
+    <button class="btn"><img scr="" alt="shirt" class="imgBtn"></img></button>
+    <button class="btn"></button> 
+    <button class="btn"></button> 
+    <button class="btn"></button> 
+{/* Items */}
     </body>
 </html>
